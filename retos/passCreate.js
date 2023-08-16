a0c2aad96952e73643d9af98a99e4bc2cd94c3b7
DeLxCruz <123611524+DeLxCruz@users.noreply.github.com>
2023-08-16 13:28:49 -0500

    siza

---
+++ retos/passCreate.js
@@ -0,0 +1,39 @@
+// Reto Password Creation
+
+//Santiago López
+
+// A password manager wants to create new passwords using two strings given by the user, then combined to create a harder-to- guess combination. Given two strings,
+// interleave the characters of the strings to create a new string. Beginning with an empty string, alternately append a character from string a and from string b. If one of the strings is exhausted before the other, append the remaining letters from the other
+// string all at once. The result is the new password.
+
+// Example:
+
+// Input: "abc", "12345"
+// Output: "a1b2c345"
+
+// Input: "abcde", "123"
+// Output: "a1b2c3de"
+
+//Solution
+
+let si = "siza";
+let sii = "mani";
+
+function passwordCreation(str1, str2) {
+    let result = "";
+    let i = 0;
+    let j = 0;
+    while (i < str1.length || j < str2.length) {
+        if (i < str1.length) {
+            result += str1[i];
+            i++;
+        }
+        if (j < str2.length) {
+            result += str2[j];
+            j++;
+        }
+    }
+    return result;
+}
+
+console.log(passwordCreation(si, sii));
